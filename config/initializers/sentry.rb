f7f6a6ff9c945a4b482a749c49e7404b63244fd8
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-11 11:30:27 +0200

    fix: Senrty config file according to suggestions in PR
    Changed sentry_key to SENTRY_DNS and manually tested; chose one way
    to trace samples.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = ENV['sentry_key']
+  config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
@@ -7,7 +7,7 @@ Sentry.init do |config|
   # We recommend adjusting this value in production
   config.traces_sample_rate = 1.0
   # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
+  # config.traces_sampler = lambda do |context|
+  #   true
+  # end
 end
