1ce6451bf093dd262ca19b8832bc2739f6885282
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-12 10:40:15 +0200

    resolve remaining style offenses

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ require 'rails/test_unit/railtie'
 Bundler.require(*Rails.groups)
 
 module MonteCinema
+  # rubocop:disable Style/Documentation
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -39,4 +40,5 @@ module MonteCinema
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
+  # rubocop:enable Style/Documentation
 end
