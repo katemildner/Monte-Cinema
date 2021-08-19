f344e35944e3c6f9a33e506fec73d51bf1d12ae6
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-19 21:38:08 +0200

    Add options null:false for roles migration; add current schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_192713) do
+ActiveRecord::Schema.define(version: 2021_08_19_193119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2021_08_19_192713) do
   end
 
   create_table "reservations", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
