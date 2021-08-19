a173ecc13b2bed1bc66b40b7a2bf4aef07bd5d61
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-19 12:54:50 +0200

    Add options null:false for CinemaHall migration; add current schema

--- db/migrate/20210819081715_create_cinema_halls.rb
+++ db/migrate/20210819081715_create_cinema_halls.rb
@@ -1,9 +1,9 @@
 class CreateCinemaHalls < ActiveRecord::Migration[6.1]
   def change
     create_table :cinema_halls do |t|
-      t.string :name
-      t.integer :number_of_rows
-      t.integer :number_of_seats_per_row
+      t.string :name, null: false
+      t.integer :number_of_rows, null: false
+      t.integer :number_of_seats_per_row, null: false
 
       t.timestamps
     end
