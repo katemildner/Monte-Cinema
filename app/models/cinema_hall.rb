a7b458e33590be9a9ce405ddd3a4c93251769c0e
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-19 13:18:50 +0200

    Add validations to CinemaHall model
    Added presence validations for all attributes and numericality
    validations for integer attributes - this will provide better
    error messages than errors directly from db.

--- app/models/cinema_hall.rb
+++ app/models/cinema_hall.rb
@@ -1,2 +1,5 @@
 class CinemaHall < ApplicationRecord
+  validates :name, presence: true
+  validates :number_of_rows, presence: true, numericality: { only_integer: true }
+  validates :number_of_seats_per_row, presence: true, numericality: { only_integer: true }
 end
