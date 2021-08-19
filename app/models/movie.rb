d91ab50cfc6c2691d16d7e6353f2b6d683cb8726
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-19 18:54:12 +0200

    Add validations to Movie model
    Added presence validations for all attributes - this will provide
    better error messages than errors directly from db.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :director, presence: true
+  validates :genre, presence: true
+  validates :duration, presence: true
 end
