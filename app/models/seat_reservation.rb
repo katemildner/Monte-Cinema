bebd64edf8fac3732a3b796c938c9159eda3ae62
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-19 21:29:17 +0200

    Add SeatReservation model, corresponding migration and updated schema

---
+++ app/models/seat_reservation.rb
@@ -0,0 +1,2 @@
+class SeatReservation < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_182409) do
+ActiveRecord::Schema.define(version: 2021_08_19_192713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 2021_08_19_182409) do
 
   create_table "screenings", force: :cascade do |t|
     t.datetime "date_and_time", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "seat_reservations", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
