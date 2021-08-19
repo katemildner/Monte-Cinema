69c540a81a8c9025eecb0759aa32c780bad92ca3
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-19 19:18:26 +0200

    Add options null:false for screenings migration; add current schema

--- db/migrate/20210819170858_create_screenings.rb
+++ db/migrate/20210819170858_create_screenings.rb
@@ -1,7 +1,7 @@
 class CreateScreenings < ActiveRecord::Migration[6.1]
   def change
     create_table :screenings do |t|
-      t.datetime :date_and_time
+      t.datetime :date_and_time, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_102639) do
+ActiveRecord::Schema.define(version: 2021_08_19_170858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2021_08_19_102639) do
     t.string "director", null: false
     t.string "genre", null: false
     t.time "duration", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "screenings", force: :cascade do |t|
+    t.datetime "date_and_time", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
