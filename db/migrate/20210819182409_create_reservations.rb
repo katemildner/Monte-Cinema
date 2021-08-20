8b029dfd22c62d42dd11089ac60de59adb18c4a1
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-20 09:16:36 +0200

    [refactor] Fix style offenses in migrations
    Excluded schema.db entirely in rubocop config, excluded migrations for Documentation cop.

--- db/migrate/20210819182409_create_reservations.rb
+++ db/migrate/20210819182409_create_reservations.rb
@@ -1,8 +1,7 @@
+# frozen_string_literal: true
+
 class CreateReservations < ActiveRecord::Migration[6.1]
   def change
-    create_table :reservations do |t|
-
-      t.timestamps
-    end
+    create_table :reservations, &:timestamps
   end
 end
--- db/migrate/20210819192713_create_seat_reservations.rb
+++ db/migrate/20210819192713_create_seat_reservations.rb
@@ -1,8 +1,7 @@
+# frozen_string_literal: true
+
 class CreateSeatReservations < ActiveRecord::Migration[6.1]
   def change
-    create_table :seat_reservations do |t|
-
-      t.timestamps
-    end
+    create_table :seat_reservations, &:timestamps
   end
 end
