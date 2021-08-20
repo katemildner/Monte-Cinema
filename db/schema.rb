17123cbdf4bf4a67ae105a41ff4e3919086fb0f8
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-20 15:57:25 +0200

    [style] Change column date_and_time name in screenings to more explicit
    For the sake of clear naming changed date_and_time to starting_date_and_time.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_193119) do
+ActiveRecord::Schema.define(version: 2021_08_20_135157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_193119) do
   end
 
   create_table "screenings", force: :cascade do |t|
-    t.datetime "date_and_time", null: false
+    t.datetime "starting_date_and_time", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
