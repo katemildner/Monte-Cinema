c317725909d6d33756db55456d1fb1b94ebea1a0
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-08-31 10:36:24 +0200

    [fix] Change date_and_time to starting_date_and_time in screening model

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Screening < ApplicationRecord
-  validates :date_and_time, presence: true
+  validates :starting_date_and_time, presence: true
 
   belongs_to :cinema_hall
   belongs_to :movie
