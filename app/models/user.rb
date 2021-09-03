9281240654006106e520bdb3c92dcf5748ffeb7b
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-09-03 17:57:36 +0200

    Add validations to User and Voucher models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates :points_earned, presence: true, numericality: { only_integer: true }
+  validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
