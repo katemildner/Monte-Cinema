7e720bc71b6be010574aee97fc5384f230b7e628
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2021-09-21 19:13:59 +0200

    Remove duplicated devise modules from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,6 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :points_earned, presence: true, numericality: { only_integer: true }
   validates :username, presence: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
   belongs_to :role
   has_many :reservations, dependent: :destroy
